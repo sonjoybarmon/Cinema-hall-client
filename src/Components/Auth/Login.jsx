6e6f302f9da916611c10736941ebaf11d87b4e2f
SONJOY BARMON <sonjoybarmon19@gmail.com>
2021-02-26 02:19:35 +0600

    complete all auth page design with metarial ui and react form hook

--- src/Components/Auth/Login.jsx
+++ src/Components/Auth/Login.jsx
@@ -1,12 +1,18 @@
-import { Container, Grid } from '@material-ui/core';
+import { Button, Container, Grid, Typography } from '@material-ui/core';
 import React from 'react';
 import { useForm } from "react-hook-form";
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox';
 import {Card} from 'ui-glassmorphism'
 import 'ui-glassmorphism/dist/index.css'
-import './Auth.scss'
+import './Login.scss'
+import { Link } from 'react-router-dom';
 
 const Login = () => {
     const { register, errors, handleSubmit } = useForm();
+
+    const [check, setCheck] = React.useState(false);
+
     const onSubmit = (data) => {
         console.log(data);
     }
@@ -18,12 +24,99 @@ const Login = () => {
                     <Grid md={6} item>
                         <Card className="login__Wrapper" >
                             <form onSubmit={handleSubmit(onSubmit)}>
-                                <input name="firstName" ref={register({ required: true })} />
-                                {errors.firstName && "First name is required"}
-                                <input name="lastName" ref={register({ required: true })} />
-                                {errors.lastName && "Last name is required"}
-                                <input type="submit" />
+                                <Typography variant='h6'>
+                                    Sign In
+                                </Typography>
+                                <div style={{margin:'10px 0'}}>
+                                    <Typography variant='subtitle1'>
+                                        Email
+                                    </Typography>
+                                    <input 
+                                        name="email" 
+                                        ref={register({ required: true , pattern: /\S+@\S+\.\S+/ })} 
+                                        className='authInput'
+                                        placeholder='Enter Your Email'
+                                    />
+                                    <Typography variant='caption' className="inputError">
+                                        {errors.email && "Please Enter a valid email"}
+                                    </Typography>
+                                    
+
+                                    <Typography variant='subtitle1'>
+                                        Password
+                                    </Typography>
+                                    <input 
+                                        name="password" 
+                                        ref={register({ required: true, minLength: 8 })} 
+                                        className='authInput'
+                                        placeholder='Enter Your Password'
+                                    />
+                                    <Typography variant='caption' className="inputError">
+                                        {errors.password && "Please Enter maximun 8 number"}
+                                    </Typography>
+                                </div>
+                                <div className='check__forgot'> 
+                                    <FormControlLabel
+                                        control={
+                                        <Checkbox
+                                            checked={check}
+                                            onChange={() => setCheck(!check)}
+                                            name="check"
+                                            color="primary"
+                                            style={{color: '#ffffff'}}
+                                        />
+                                        }
+                                        label="Remember Me"
+                                    />
+                                    <Link to='/forgotPassword' className='linkStyle'>
+                                        <Typography variant='subtitle2'>
+                                            Forgot Password?
+                                        </Typography>
+                                    </Link>
+                                </div>
+                                       
+                                <Button 
+                                    variant='contained' 
+                                    color='primary' 
+                                    type="submit"
+                                    className='authButton'
+                                > 
+                                    Sign In
+                                </Button>
+
+                                <Typography variant='subtitle2'>
+                                Don't have account? 
+                                <Link to='/signup' style={{color : '#fff', marginLeft: '6px'}}>
+                                    SignUp
+                                </Link>
+                                </Typography>
+                                {/* <input type="submit"/> */}
                             </form>
+
+                            <div className='facebook__Google'>
+                                <Typography variant='subtitle1' style={{margin: '5px'}}>
+                                    Or Sign in using google or facebook
+                                </Typography>
+
+                                <div className='ButtonSection'>
+                                    <Button 
+                                        variant='contained'
+                                        color= 'primary'
+                                        className='google btn' 
+                                        onClick={()=>alert('fb')}
+                                    >
+                                        Continue with Facebook
+                                    </Button> <br />
+                                    <Button 
+                                        variant='contained'
+                                        color= 'primary'
+                                        className='facebook btn' 
+                                        onClick={()=>alert('Google')}
+                                    >
+                                        Continue with Google
+                                    </Button>
+                                </div>
+                            </div>
                         </Card>
                     </Grid>
                 </Grid>
